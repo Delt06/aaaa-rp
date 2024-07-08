7918f5d8446c755e50f1a7d58c2e2255e6603f82
Vladislav Kantaev <vvkantaev3@gmail.com>
2024-07-09 00:00:20 +0200

    Add masking when storing indexIF

--- Packages/com.deltation.aaaa-rp/ShaderLibrary/VisibilityBuffer/Utils.hlsl
+++ Packages/com.deltation.aaaa-rp/ShaderLibrary/VisibilityBuffer/Utils.hlsl
@@ -2,11 +2,11 @@
 #define AAAA_VISIBILITY_BUFFER_UTILS_INCLUDED
 
 #define VISIBILITY_BUFFER_INDEX_ID_BITS (8u)
-#define VISIBILITY_BUFFER_INDEX_ID_MASK ((1u << VISIBILITY_BUFFER_INDEX_ID_BITS) - 1u) 
+#define VISIBILITY_BUFFER_INDEX_ID_MASK ((1u << VISIBILITY_BUFFER_INDEX_ID_BITS) - 1u)
 
 uint2 PackVisibilityBufferValue(const uint instanceID, const uint meshletID, const uint indexID)
 {
-    return uint2(instanceID, meshletID << VISIBILITY_BUFFER_INDEX_ID_BITS | indexID / 3);
+    return uint2(instanceID, meshletID << VISIBILITY_BUFFER_INDEX_ID_BITS | indexID / 3 & VISIBILITY_BUFFER_INDEX_ID_MASK);
 }
 
 void UnpackVisibilityBufferValue(uint2 value, out uint instanceID, out uint meshletID, out uint indexID)
